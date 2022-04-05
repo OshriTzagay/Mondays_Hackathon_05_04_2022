0c62ff0337109cdb9035b28c623e6130255139ac
ermias-mangisto <ermiasmangisto@gmail.com>
2022-04-06 00:11:54 +0300

    .

--- client/src/Components/Features/navbar/Navbar-component.jsx
+++ client/src/Components/Features/navbar/Navbar-component.jsx
@@ -6,11 +6,11 @@ import { UsersContext } from "../../../Context/User/User-context";
 export default function Navbar() {
   const { user, setUser } = useContext(UsersContext);
   return (
-    <div>
-      <Link  to="/" className="nav-links">LOGIN||</Link>
-      <Link  to="home" className="nav-links">HOME||</Link>
-      <Link  to="ideas"className="nav-links">POSTS||</Link>
-      <Link  to={`profile/${user._id}`}className="nav-links">PROFILE||</Link>
+    <div className="the-navbar">
+      <Link className="nav-links" to="/" >LOGIN||</Link>
+      <Link  className="nav-links" to="home">HOME||</Link>
+      <Link className="nav-links" to="ideas">POSTS||</Link>
+      <Link className="nav-links" to={`profile/${user._id}`}>PROFILE||</Link>
     </div>
     
   );
