db41bfa606d025089c317928db8d4625d98ff7fb
kerenhailu <keren4509@gmail.com>
2022-04-05 22:30:01 +0300

    profile

--- client/src/Components/Pages/Profile/Profile-component.jsx
+++ client/src/Components/Pages/Profile/Profile-component.jsx
@@ -1,15 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { UsersContext } from "../../../Context/User/User-context";
-import { GetIdeaById } from "../../../Services/Ideas-Service";
 import { GetUserById } from "../../../Services/User-Service";
 import ProfileDetails from "./Profile-Details"
-// import { GetAll } from '../../../Services/User-Service';
 
 export default function Profile() {
-  const { user, setUser } = useContext(UsersContext);
   const {id}=useParams()
-  const [userIdea, setUserIdea] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
   useEffect(() =>{
       const loadCurrentUser=  async ()=>{
@@ -17,7 +12,6 @@ export default function Profile() {
         setCurrentUser(newUser.Chosen_User);
       }
       loadCurrentUser()
-
   } , []);
 
   return (
