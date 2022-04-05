eb7c95469e45624cae4db78edefdcb73becae448
kerenhailu <keren4509@gmail.com>
2022-04-05 18:23:30 +0300

    profile

--- client/src/Components/Pages/Profile/Profile-component.jsx
+++ client/src/Components/Pages/Profile/Profile-component.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GetAll } from '../../../Services/User-Service';
 
 export default function Profile() {
+  let [userData, setUserData] = useState([]);
+  const user = JSON.parse(localStorage.getItem('user'));
+  useEffect(() => {
+    GetAll()
+      .then(data => setUserData(data))
+  } , []); 
   return (
-    <div>Profile</div>
+    <div>Profile
+      <img src={user.ProfilePic} alt="user Pic" />
+      <h1>{user.FirstName} {user.LastName}</h1>
+      <h3>{user.Age}</h3>
+      <h3>{user.Email}</h3>
+      <h3>{user.Position}</h3>
+      
+{/* FirstName,LastName,Age,Ideas, Email,Position,Password,Rank,LastLogin,isLogin,isAdmin,Role,ProfilePic, */}
+
+
+
+    </div>
   )
 }
