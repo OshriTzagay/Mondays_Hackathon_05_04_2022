eea871b83ea16f28db17a76243c1d9a2e7ef6604
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-04-06 02:45:13 +0300

    admin component

--- client/src/Components/Features/navbar/Navbar-component.jsx
+++ client/src/Components/Features/navbar/Navbar-component.jsx
@@ -12,27 +12,30 @@ import { CgProfile } from 'react-icons/cg';
 
 
 export default function Navbar() {
-const navigate = useNavigate();
-const { user, setUser,setIsLoggedIn } = useContext(UsersContext);
-const LogOut = ()=>{
-  localStorage.removeItem("token")
-  setUser({})
-  setIsLoggedIn(false)
-  navigate("/")
-}
+  const navigate = useNavigate();
+  const { user, setUser, setIsLoggedIn } = useContext(UsersContext);
+  const LogOut = () => {
+    localStorage.removeItem("token")
+    setUser({})
+    setIsLoggedIn(false)
+    navigate("/")
+  }
 
   return (
     <div className="the-navbar">
       <div className="left-nav">
-      <Link className="nav-links" to="home">ICON</Link>
-      <Link className="nav-links" to="home">HOME</Link>
-      {/* <Link className="nav-links" to="/">LOGIN</Link> */}
-      <Link className="nav-links" to="ideas">POSTS</Link>
+        <Link className="nav-links" to="home">ICON</Link>
+        <Link className="nav-links" to="home">HOME</Link>
+        {user.isAdmin ?
+          <Link className="nav-links" to="admin">Admin||</Link>
+          : ''}
+        {/* <Link className="nav-links" to="/">LOGIN</Link> */}
+        <Link className="nav-links" to="ideas">POSTS</Link>
       </div>
       <div className="right-nav">
 
-      <Link className="nav-links" to={`profile/${user._id}`}><CgProfile/></Link>
-      <Link className="nav-links" to="/" onClick={LogOut}><MdLogout/></Link>
+        <Link className="nav-links" to={`profile/${user._id}`}><CgProfile /></Link>
+        <Link className="nav-links" to="/" onClick={LogOut}><MdLogout /></Link>
 
       </div>
 
