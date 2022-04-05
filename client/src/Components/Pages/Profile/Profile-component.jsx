d73e878ed372a60fd0cb5b02009e79134953aada
kerenhailu <keren4509@gmail.com>
2022-04-05 19:05:32 +0300

    profile

--- client/src/Components/Pages/Profile/Profile-component.jsx
+++ client/src/Components/Pages/Profile/Profile-component.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UsersContext } from '../../../Context/User/User-context';
 import { GetIdeaById } from '../../../Services/Ideas-Service';
 // import { GetAll } from '../../../Services/User-Service';
 
 export default function Profile() {
-  let [userIdea, setUserIdea] = useState([]);
-  const user = JSON.parse(localStorage.getItem('user'));
-  useEffect(() => {
-    GetIdeaById(user.id)
-      .then(Idea => setUserIdea(Idea))
-  } , []); 
+  const { user, setUser } = useContext(UsersContext);
+  // let [userIdea, setUserIdea] = useState([]);
+  // const user = JSON.parse(localStorage.getItem('user'));
+  // useEffect(() => {
+  //   GetIdeaById(user.id)
+  //     .then(Idea => setUserIdea(Idea))
+  // } , []); 
+  console.log(user);
   return (
     <div>Profile
       <img src={user.ProfilePic} alt="user Pic" />
@@ -17,15 +20,9 @@ export default function Profile() {
       <h3>{user.Email}</h3>
       <h3>{user.Position}</h3>
 
-      post : {userIdea}
-
-
-
-
-      
+      {/* post : {userIdea} */}
+   
 {/* FirstName,LastName,Age,Ideas, Email,Position,Password,Rank,LastLogin,isLogin,isAdmin,Role,ProfilePic, */}
-
-
 
     </div>
   )
