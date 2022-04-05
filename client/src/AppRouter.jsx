aaf1c8225a9379bc8a36689b6b6259cd018f8512
kerenhailu <keren4509@gmail.com>
2022-04-06 00:56:30 +0300

    login

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -8,31 +8,30 @@ import NavBar from "./Components/Features/NavBar/Navbar-component";
 import Footer from "./Components/Features/Footer/Footer-component";
 import NotFound from "./Components/Pages/NotFound/NotFound-component";
 import { UsersContext } from "./Context/User/User-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export const AppRouter = () => {
   const { isLoggedIn, setIsLoggedIn } = useContext(UsersContext);
     setIsLoggedIn(false);
+  
   return (
     <div className="App">
       <BrowserRouter>
-    
-        <NavBar />
+      {isLoggedIn? <NavBar />  : null}
+       
         <div className="main-content">
           <Routes>
             <Route exact path="/" element={<Login />}></Route>
-
             <Route exact path="/home" element={<Home />}> </Route>
             <Route exact path="/ideas" element={<Ideas />}>
             <Route exact path=":prodact" element={<IdeasByCategory />}></Route>
             </Route>
             <Route exact path="/profile/:id" element={<Profile />}></Route>
               <Route exact path="*" element={<NotFound />}></Route>
-
-
           </Routes>
         </div>
-    <Footer />
+      {isLoggedIn?<Footer />: null}
+    
       </BrowserRouter>
   
 
