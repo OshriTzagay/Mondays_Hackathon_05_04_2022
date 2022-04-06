96f236f537709e9e7b8840682d9441dcd865df9a
OshriTzagay <oshritzagay@gmail.com>
2022-04-06 13:56:48 +0300

    /

--- client/src/Components/Pages/Ideas/IdeasByCategory-Component.jsx
+++ client/src/Components/Pages/Ideas/IdeasByCategory-Component.jsx
@@ -1,28 +1,35 @@
-import React,{useState,useEffect} from 'react'
-import {GetIdeaByType} from "../../../Services/Ideas-Service" ;
+import React, { useState, useEffect } from "react";
+import { GetIdeaByType } from "../../../Services/Ideas-Service";
 import { useParams } from "react-router-dom";
-import Idea from "./Idea"
-import TopFive from '../../Features/TopFive/TopFive';
+import Idea from "./Idea";
+import TopFive from "../../Features/TopFive/TopFive";
 export default function IdeasByCategory() {
-  const {type}=useParams()
-const [ideas,setIdeas]=useState([])
-useEffect(()=>{ 
+  const { type } = useParams();
+  const [ideas, setIdeas] = useState([]);
+  useEffect(() => {
     const loadIdeas = async () => {
       const Ideas = await GetIdeaByType(type);
       setIdeas(Ideas);
-    };   
+    };
     loadIdeas();
     console.log(type);
-},[])
+  }, []);
 
-  return (<div className='ideasAndTop'>
-    <div className='Top'><TopFive/></div>
+  return (
+    <div className="ideas-container">
+      <div className="Top">
+        <TopFive />
+      </div>
 
-
-    <div className="ideasHolder">{ideas.map((idea,i)=>{
-
-return <h1 key={i}><Idea idea={idea}/></h1>
-    })}</div>
+      <div className="ideasHolder">
+        {ideas.map((idea, i) => {
+          return (
+            <div className="ideasCards" key={i}>
+              <Idea idea={idea} />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
