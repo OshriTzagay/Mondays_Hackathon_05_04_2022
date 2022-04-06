bf57043079d68481d5ff29d758e80bb48a193918
OshriTzagay <oshritzagay@gmail.com>
2022-04-06 14:24:36 +0300

    pushtoMain

--- client/src/Components/Pages/Ideas/Ideas-component.jsx
+++ client/src/Components/Pages/Ideas/Ideas-component.jsx
@@ -1,24 +1,38 @@
-
-import React from 'react';
+import React from "react";
 // import "./Idea.css"
-import { Link ,Outlet} from "react-router-dom";
-import StickyLight from '../../Features/StickyLight/StickLight-component';
-import './ideasComponent.css'
+import { Link, Outlet } from "react-router-dom";
+import StickyLight from "../../Features/StickyLight/StickLight-component";
+import "./ideasComponent.css";
 export default function Ideas() {
   return (
-    <div className='ideas-container'>
-      <div className='heroImg'></div>
+    <div className="ideas-container">
+      <div className="heroImg"></div>
       <div className="inner-nav">
-      <StickyLight/>
+        <StickyLight />
 
-
-  <div><Link to="prodact"><h1>Products</h1></Link></div>  
-   <div> <Link to="BusinessExcellence"><h1>Business Excellence</h1></Link></div>
-   <div><Link to="WorkFlow"><h1>Work Flow</h1></Link></div> 
-   <div> <Link to="SocialImpact"><h1>Social-Impact</h1></Link></div>
-      
+        <div className="categories">
+          <Link to="prodact">
+            <h1>Products</h1>
+          </Link>
+        </div>
+        <div className="categories">
+          {" "}
+          <Link to="BusinessExcellence">
+            <h1>Business Excellence</h1>
+          </Link>
+        </div>
+        <div className="categories">
+          <Link to="WorkFlow">
+            <h1>Work Flow</h1>
+          </Link>
+        </div>
+        <div className="categories">
+          {" "}
+          <Link to="SocialImpact">
+            <h1>Social-Impact</h1>
+          </Link>
+        </div>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
